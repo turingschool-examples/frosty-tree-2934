3e54695288db89cafd0f3dc1cf94ef91d90bc061
Stephen Nash <stephen.aa.nash@gmail.com>
2023-12-15 13:16:16 -0500

    Create test and view partially

--- spec/features/guest/guest_show_spec.rb
+++ spec/features/guest/guest_show_spec.rb
@@ -23,4 +23,23 @@ RSpec.describe "the guest show page", type: :feature do
     # and the name of the hotel that it belongs to.
     expect(page).to have_content(hotel_1.name)
   end
+  
+  it "creates a form to add a guest to a room" do
+    hotel_1 = Hotel.create!(name: 'Echo Mountain Inn', location: 'Echo Mountain')
+    room_1 = hotel_1.rooms.create!(rate: 125, suite: "Presidential")
+    room_2 = hotel_1.rooms.create!(rate: 5, suite: "Squatter")
+    guest_1 = Guest.create!(name: 'Charlize Theron', nights: 3)
+  
+    # When I visit a guest's show page
+    visit "/guests/#{guest_1.id}"
+    save_and_open_page
+    # Then I see a form to add a room to this guest.
+    expect(page).to have_content(room_1.id)
+
+    # When I fill in a field with the id of an existing room
+    # And I click submit
+    # Then I am redirected back to the guest's show page
+    # And I see the room now listed under this guest's rooms.
+    # (You do not have to test for a sad path, for example if the ID submitted is not an existing room)
+  end
 end
