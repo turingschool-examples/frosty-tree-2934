fc80f0ff1f8aa6a86607e8d14546da5f7bb43642
Stephen Nash <stephen.aa.nash@gmail.com>
2023-12-15 11:23:30 -0500

    Add some more initial framework

---
+++ app/controllers/guests_controller.rb
@@ -0,0 +1,38 @@
+class  GuestsController < ApplicationController
+
+  def index
+
+  end
+
+  # def show
+  #   @? = ?.find(params[:id])
+  # end
+
+  # def new
+  #   @? = ?.find(params[:shelter_id])
+  # end
+
+  # def create
+  #    = .new(_params)
+  # end
+
+  # def edit
+  #   @? = .find(params[:id])
+  # end
+
+  # def update
+  #    = .find(params[:id])
+  # end
+
+  # def destroy
+  #   .find(params[:id]).destroy
+  #   redirect_to "/"
+  # end
+
+  # private
+
+  # def _params
+  #   params.permit(:name)
+  # end
+
+end
---
+++ spec/models/guest_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe Guest, type: :model do
+
+  describe "relationships" do
+    it { should have_many(:rooms) }
+  end
+
+  # describe "" do
+  #   it "" do
+  #     expect().to eq()
+  #   end
+  # end
+
+end
