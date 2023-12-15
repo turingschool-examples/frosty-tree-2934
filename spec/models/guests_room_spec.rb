f76fa9637a123368550bd679d2db63e216538436
Barry Allahyar <barrya@gmail.com>
2023-12-15 13:13:10 -0600

    Ahhhh, time's up! Uploading everything I have although tests not passing >.<

--- spec/models/guests_room_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe GuestsRoom, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
--- spec/models/hotel_spec.rb
+++ spec/models/hotel_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe Hotel, type: :model do
   describe "relationships" do
     it { should have_many :rooms }
-    it { should have_many :guests }
+    it { should have_many(:guests).through(:rooms) }
   end
 
   
