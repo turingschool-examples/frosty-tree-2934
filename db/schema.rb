452c66e53570e1d857c61031b4a5b336fb81c407
Nathan Lambertson <lambertones@gmail.com>
2023-12-15 10:48:23 -0600

    Finally get tables right, maybe

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_15_161845) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_15_163746) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,12 +35,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_15_161845) do
     t.bigint "hotel_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "room_id", null: false
+    t.bigint "guest_id", null: false
+    t.index ["guest_id"], name: "index_rooms_on_guest_id"
     t.index ["hotel_id"], name: "index_rooms_on_hotel_id"
-    t.index ["room_id"], name: "index_rooms_on_room_id"
   end
 
   add_foreign_key "guests", "rooms"
+  add_foreign_key "rooms", "guests"
   add_foreign_key "rooms", "hotels"
-  add_foreign_key "rooms", "rooms"
 end
--- spec/models/guest_spec.rb
+++ spec/models/guest_spec.rb
@@ -4,6 +4,5 @@ RSpec.describe Guest, type: :model do
   describe "relationships" do
     it { should have_many :rooms }
     it { should belong_to :room }
-    it { should belong_to :hotel }
   end
 end
