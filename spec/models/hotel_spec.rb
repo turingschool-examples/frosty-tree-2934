c7cbb67f2883fac5c8beb521cc248422f401cfd2
Barry Allahyar <barrya@gmail.com>
2023-12-15 11:43:43 -0600

    Create initial models, setup relationships, and ran migration

--- spec/models/hotel_spec.rb
+++ spec/models/hotel_spec.rb
@@ -3,5 +3,8 @@ require "rails_helper"
 RSpec.describe Hotel, type: :model do
   describe "relationships" do
     it { should have_many :rooms }
+    it { should have_many :guests }
   end
+
+  
 end
