e820e895018cd42af390a5ba63bee2b1408b3ebd
Sam Puttman <daputter2@gmail.com>
2023-12-15 12:45:30 -0500

    refactor guests_controller and routes, removing lines that are unnecessary for the user stories.

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -4,14 +4,6 @@ class GuestsController < ApplicationController
     @guest = Guest.find(params[:id])
   end
 
-  def index
-    @guests = Guest.all
-  end
-
-  def new
-    @guest = Guest.new
-  end
-
   def add_room
     @guest = Guest.find(params[:id])
     @guest.add_room_by_id(params[:room_id])
