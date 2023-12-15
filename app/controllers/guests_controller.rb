4ad226b21ec456ce25d1613221b372cfd34b2b55
Sam Puttman <daputter2@gmail.com>
2023-12-15 12:15:22 -0500

    Add add_room model method and implement in controller to match user story 2

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -11,4 +11,11 @@ class GuestsController < ApplicationController
   def new
     @guest = Guest.new
   end
+
+  def add_room
+    @guest = Guest.find(params[:id])
+    @guest.add_room_by_id(params[:room_id])
+
+    redirect_to "guests/#{@guest.id}"
+  end
 end
