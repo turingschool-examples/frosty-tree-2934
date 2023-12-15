cbde14e51382ce372e46c875eda47625b80542ff
Meg Roth <megroth2@gmail.com>
2023-12-15 11:10:38 -0700

    Add user story 3, rooms index page

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,4 +3,8 @@ class Room < ApplicationRecord
   has_many :guest_rooms
   has_many :guests, through: :guest_rooms
 
+  def total_guests
+    self.guests.count # Is this an AR method or is this ruby??
+  end
+
 end
--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -6,4 +6,29 @@ RSpec.describe Room, type: :model do
     it { should have_many :guest_rooms }
     it { should have_many :guests }
   end
+
+  before(:each) do 
+    @hotel_1 = Hotel.create!(name: "Marriott", location: "Silverthorne")
+    @hotel_2 = Hotel.create!(name: "Hilton", location: "Breckenridge")
+
+    @room_1 = Room.create!(rate: 200, suite: "Presidential", hotel_id: @hotel_1.id)
+    @room_2 = Room.create!(rate: 100, suite: "Deluxe", hotel_id: @hotel_1.id)
+    @room_3 = Room.create!(rate: 90, suite: "Standard", hotel_id: @hotel_2.id)
+
+    @guest_1 = Guest.create!(name: "Michael Scott", nights: 1)
+    @guest_2 = Guest.create!(name: "Dwight Schrute", nights: 2)
+    @guest_3 = Guest.create!(name: "Jim Halpert", nights: 3)
+
+    @guest_room_1 = GuestRoom.create!(guest_id: @guest_1.id, room_id: @room_1.id)
+    @guest_room_2 = GuestRoom.create!(guest_id: @guest_2.id, room_id: @room_1.id)
+    @guest_room_3 = GuestRoom.create!(guest_id: @guest_2.id, room_id: @room_2.id)
+    @guest_room_4 = GuestRoom.create!(guest_id: @guest_3.id, room_id: @room_3.id)
+  end
+
+  it "displays total guests" do
+    expect(@room_1.total_guests).to eq(2)
+    expect(@room_2.total_guests).to eq(1)
+    expect(@room_3.total_guests).to eq(1)
+  end
+
 end
