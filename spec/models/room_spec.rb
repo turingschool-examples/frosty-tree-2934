c59f156030377b02d24a72e16de567c2f43d063c
Quin Nordmark <quinnordmark@gmail.com>
2023-12-15 10:00:28 -0700

    models setup

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -4,4 +4,11 @@ RSpec.describe Room, type: :model do
   describe "relationships" do
     it { should belong_to :hotel }
   end
+
+  describe "validations" do
+    it {should validate_presence_of(:rate)}
+    it {should validate_numericality_of(:rate)}
+    it {should validate_presence_of(:suite)}
+    it {should validate_presence_of(:suite)}
+  end
 end
