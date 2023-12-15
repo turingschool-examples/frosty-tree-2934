8fd3ea78434d4913d04c59b96fb74c76f53f499c
Dylan Perry <dylanbperry@gmail.com>
2023-12-15 08:17:35 -0800

    Add basic existence and name/location attribute tests

--- spec/models/hotel_spec.rb
+++ spec/models/hotel_spec.rb
@@ -4,4 +4,14 @@ RSpec.describe Hotel, type: :model do
   describe "relationships" do
     it { should have_many :rooms }
   end
+
+  describe "class methods" do
+    it "should exist, and have a name and location" do
+    inn = Hotel.new(name: "Doubletree Inn", location: "Barstow")
+    
+    expect(inn).to be_a Hotel
+    expect(inn.name).to eq("Doubletree Inn")
+    expect(inn.location).to eq("Barstow")
+    end
+  end
 end
