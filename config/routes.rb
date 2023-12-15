e6c16672cfd5f502d8bb47fa754db6c723bde468
Sam Puttman <daputter2@gmail.com>
2023-12-15 11:29:42 -0500

    add routes for guests for US#1-2 and room index for US#3

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "guests", to: "guests#index"
+  get "guests/:id", to: "guests#show"
+  get "guests/new", to: "guests#new"
+  post "guests", to: "guests#create"
+
+  get "rooms", to: "rooms#index"
 end
