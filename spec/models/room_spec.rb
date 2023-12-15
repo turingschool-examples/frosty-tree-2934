42c185c532c9f6cbdc9422c2902979d34e874230
Dylan Perry <dylanbperry@gmail.com>
2023-12-15 08:20:36 -0800

    Add basic existence test and attribute tests for rate and suite

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -4,4 +4,14 @@ RSpec.describe Room, type: :model do
   describe "relationships" do
     it { should belong_to :hotel }
   end
+
+  describe "class methods" do
+    it "should exist, and have a rate and suite" do
+    room = Room.new(rate: 450, suite: "Honeymoon")
+    
+    expect(room).to be_a Room
+    expect(room.rate).to eq(450)
+    expect(room.suite).to eq("Honeymoon")
+    end
+  end
 end
