2f320f8908c0dfbdf25c6050b86b846f50b6872d
Nathan Lambertson <lambertones@gmail.com>
2023-12-15 12:58:29 -0600

    Finish the test for US1 and a little bit of implementation

--- spec/features/guests/show_spec.rb
+++ spec/features/guests/show_spec.rb
@@ -3,9 +3,14 @@ require "rails_helper"
 RSpec.describe "Guest show page", type: :feature do
   it "shows guest name and a list of rooms they've stayed in, and the room's attributes, including hotel it belongs to" do
     hotel1 = Hotel.create!(name: "Hotel 6", location: "San Francisco")
-    guest1 = Guest.create!(name: "Fred")
-    room23 = Room.create!(rate: 45, suite: "Love-Bugs")
+    room23 = hotel1.rooms.create!(rate: 45, suite: "Love-Bugs")
+    guest1 = room23.guests.create!(name: "Fred")
 
+    visit "/guests/#{guest1.id}"
 
+    expect(current_path).to eq("/guests/#{guest1.id}")
+    expect(page).to have_content(guest1.name)
+    expect(page).to have_content(room23.suite)
+    expect(page).to have_content(hotel1.name)
   end
 end
