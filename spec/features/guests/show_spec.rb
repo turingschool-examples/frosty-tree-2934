a2d07179393202c11329711674192f0bc7c9628b
Yain Porter <144638203+yainporter@users.noreply.github.com>
2023-12-15 09:58:18 -0700

    test: Write the test for US1

--- spec/features/guests/show_spec.rb
+++ spec/features/guests/show_spec.rb
@@ -1,5 +1,29 @@
 require "rails_helper"
 
 RSpec.describe "Guest Show Page", type: :feature do
+  before(:each) do
+    @rob = Guest.create!(name: "Rob", nights: 4)
+    @turing = Hotel.create!(name: "Turing", location: "Colorado")
+    @fsa = Hotel.create!(name: "Fullstack Academy", location: "New York")
+    @turing_single_suite = Room.create!(rate: 130, suite: "Single")
+    @fsa_single_suite = Room.create!(rate: 150, suite: "Single")
+  end
+  describe "User Story 1" do
+    it "has information on the guest and their stays" do
+      visit "/guests/#{@rob.id}"
 
+      expect(page).to have_content("Rob")
+      expect(page).to have_content("Rooms Stayed In:")
+
+      within ("rooms-stayed-in:") do
+        expect(page).to have_content("Room Suite: Single")
+        expect(page).to have_content("Nightly Rate: 130")
+        expect(page).to have_content("Name of Hotel: Turing")
+
+        expect(page).to have_content("Room Suite: Single")
+        expect(page).to have_content("Nightly Rate: 150")
+        expect(page).to have_content("Name of Hotel: Fullstack Academy")
+      end
+    end
+  end
 end
