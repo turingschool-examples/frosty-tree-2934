b817a1a568895dc5cc491bbb410d44ecded901f7
Dylan Perry <dylanbperry@gmail.com>
2023-12-15 10:27:39 -0800

    Add tests and functionality to show room attributes on guest show page

--- spec/features/guests/show_spec.rb
+++ spec/features/guests/show_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe "the guest show" do
         @holiday_inn = Hotel.create(name: "Holiday Inn", location: "Mars")
         @honeymoon_suite = @holiday_inn.rooms.create(rate: 25, suite: "Honeymoon")
         @florence = @honeymoon_suite.guests.create(name: "Florence Pugh", nights: 12)
+        @oval_suite = @holiday_inn.rooms.create(rate: 100, suite: "Oval")
+        @oval_suite.add_guest_to_room(@florence.id)
     end
     
     it "shows the guest and all its attributes" do
@@ -17,7 +19,8 @@ RSpec.describe "the guest show" do
     it "shows the guest's rooms, including the rooms' suite, rate, and hotel" do
         visit "/guests/#{@florence.id}"
 
-        expect(page).to have_content("Guest: #{@florence.name}")
-        expect(page).to have_content("Nights: #{@florence.nights}")
+        expect(page).to have_content("Suite Hotel: #{@oval_suite.hotel.name}")
+        expect(page).to have_content("Suite Name: #{@oval_suite.suite}")
+        expect(page).to have_content("Suite Rate: #{@oval_suite.rate}")
     end
 end
